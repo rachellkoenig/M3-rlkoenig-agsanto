860ac50245c50bbcdff999ac4d91e4624c5dfa91
rachellkoenig <rachel.koenig@uvm.edu>
2023-10-29 19:19:11 -0400

    Opened files in python and stored them

--- graph_generator.py
+++ graph_generator.py
@@ -3,5 +3,10 @@ import sys
 import numpy as np
 
 # Store command line arguments
-reads = sys.argv[1]
-writes = sys.argv[2]
+reads_file = sys.argv[1]
+writes_file = sys.argv[2]
+
+# Open the file
+reads = open(reads_file, "r")
+writes = open(writes_file, "r")
+
