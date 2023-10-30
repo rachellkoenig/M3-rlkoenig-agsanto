76b333aae76816398f39729229dfff77e40cf467
alexissanto <agsanto@uvm.edu>
2023-10-29 23:11:45 -0400

    twoSort.h code functionality and style updated

--- twoSort.h
+++ twoSort.h
@@ -17,25 +17,39 @@ vector<Comparable> firstSort(vector<Comparable>& vec, int& reads, int& writes) {
 }
 
 // Second Sort - Insertion Sort
-void secondSort(vector<NHLDraft> vec) {
+void secondSort(vector<NHLDraft> vec, int& reads, int& writes) {
     int unsortedStartIndex, insertIndex;
     NHLDraft toBeInserted;
     for (unsortedStartIndex = 1; unsortedStartIndex < vec.size(); ++unsortedStartIndex) {
-        toBeInserted = vec[unsortedStartIndex];
+        toBeInserted = vec[unsortedStartIndex]; // 1 read, 1 write
+        // Update read and write count
+        reads += 1;
+        writes +=1;
         // Loop to shift over the larger elements
         insertIndex = unsortedStartIndex - 1;
-        while (insertIndex >= 0 && vec[insertIndex].getYear() < toBeInserted.getYear()) {
-            vec[insertIndex + 1] = vec[insertIndex];
+        while (insertIndex >= 0 && vec[insertIndex].getYear() < toBeInserted.getYear()) {   // 2 reads
+            // Update read count
+            reads += 2;
+            vec[insertIndex + 1] = vec[insertIndex];    // 1 read, 1 write
+            // Update read and write count
+            reads += 1;
+            writes +=1;
             --insertIndex;
         }
+        // Even if false, the second while loop condition counts as 2 reads
+        reads += 2;
         // Put toBeInserted back into vec
-        vec[insertIndex + 1] = toBeInserted;
+        vec[insertIndex + 1] = toBeInserted;    // 1 read, 1 write
+        // Update read and write count
+        reads += 1;
+        writes +=1;
     }
 }
 
+// Combine firstSort and secondSort to make twoSort
 template<typename Comparable>
 void twoSort(vector<Comparable> vec, int& reads, int& writes) {
-    secondSort(firstSort(vec, reads, writes));
+    secondSort(firstSort(vec, reads, writes), reads, writes);
 }
 
 #endif //PROJECT4_TWOSORT_H
